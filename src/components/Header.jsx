3bb577de2a18fab22c468530c67476f58f51a188
Tommaso Pifferi <tommaso.pifferi@prima.it>
2021-01-03 18:27:28 +0100

    Add nav in header, logo and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,37 @@
 import { h } from "preact";
 
+function getLogo() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    return "/assets/images/logo_dark_mode.png";
+  }
+
+  return "/assets/images/logo_light_mode.png";
+}
+
 const Header = () => {
   return (
     <header>
+      <nav>
+        <a href="/">
+          <img alt="JPEG.rocks" src={getLogo()} class="logo" />
+        </a>
+
+        <ul>
+          <li>
+            <a
+              href="https://github.com/neslinesli93/jpeg.rocks"
+              target="_blank"
+              rel="noreferrer"
+            >
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
+
       <h1 className="text-center">Privacy-aware JPEG optimizer</h1>
 
       <p className="text-center">
