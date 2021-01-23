c83ab2d11e1faddb43150e7b38024b24c6c37c46
Tommaso <p.tommy93@gmail.com>
2021-01-23 15:24:09 +0100

    Use new conversion API

--- src/converter/wasm.worker.js
+++ src/converter/wasm.worker.js
@@ -25,41 +25,51 @@ export class JpegConverter {
     );
     this.Module.HEAPU8.set(input, inputBuffer);
 
-    const convert = this.Module.cwrap("convert", "number", [
-      "number",
-      "number",
-      "number",
-      "number",
-    ]);
-    const result = convert(inputBuffer, input.length, quality, orientation);
-    let resultData = null;
-    let resultSize = null;
-
     // Deal with possibly different versions of the WASM module:
-    // 1) [Deprecated] Returns a string with "pointer|size"
-    // 2) [New] Returns a struct with two u32 fields: pointer and size
-    if (typeof result === "string") {
-      const splitted = result.split("|");
-      const outputBuffer = parseInt(splitted[0], 16);
-      resultSize = parseInt(splitted[1], 10);
+    // 1) [New] Returns a struct with two u32 fields: pointer and size
+    // 2) [Deprecated] Returns a string with "pointer|size"
+    try {
+      console.log("new convert");
+      const convert = this.Module.cwrap("new_convert", "number", [
+        "number",
+        "number",
+        "number",
+        "number",
+      ]);
+      const result = convert(inputBuffer, input.length, quality, orientation);
 
-      resultData = new Uint8Array(
-        this.Module.HEAPU8.subarray(outputBuffer, outputBuffer + resultSize)
-      );
-
-      this.Module._free(inputBuffer);
-    } else {
       const outputBuffer = this.Module.HEAPU32[result / 4];
-      resultSize = this.Module.HEAPU32[result / 4 + 1];
+      const resultSize = this.Module.HEAPU32[result / 4 + 1];
 
-      resultData = new Uint8Array(
+      const resultData = new Uint8Array(
         this.Module.HEAPU8.subarray(outputBuffer, outputBuffer + resultSize)
       );
 
       this.Module._free(inputBuffer);
       this.Module._free(outputBuffer);
-    }
 
-    return { resultData, resultSize };
+      return { resultData, resultSize };
+    } catch (error) {
+      console.log("old convert");
+      const convert = this.Module.cwrap("convert", "string", [
+        "number",
+        "number",
+        "number",
+        "number",
+      ]);
+      const result = convert(inputBuffer, input.length, quality, orientation);
+
+      const splitted = result.split("|");
+      const outputBuffer = parseInt(splitted[0], 16);
+      const resultSize = parseInt(splitted[1], 10);
+
+      const resultData = new Uint8Array(
+        this.Module.HEAPU8.subarray(outputBuffer, outputBuffer + resultSize)
+      );
+
+      this.Module._free(inputBuffer);
+
+      return { resultData, resultSize };
+    }
   }
 }
